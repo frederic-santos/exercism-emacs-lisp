3a3e31d97ab26ce9e0f5f35e41ad7c8e1b002b1d
f-santos <frederic.santos@u-bordeaux.fr>
2020-06-22 19:48:42 +0200

    New version of acronym

--- acronym/acronym.el
+++ acronym/acronym.el
@@ -1,37 +1,23 @@
 ;;; acronym.el --- Acronym (exercism)
 
 ;;; Commentary:
-;;; First try, probably not optimal. The function proceeds as follows:
-;;; 1. Create a temp buffer and copy the phrase into it.
-;;; 2. Initialize the result with the first letter of the phrase.
-;;; 3. Add to the result the first (capitalized) letter of each word.
-;;; 4. Reverse the string (since with `cons' the last word is first in the result).
-;;; 5. Turn the list of characters (i.e., integers) into a string.
+;;; There is a repeated call (string-match "[ -]+" current-phrase)
+;;; I've (not yet) found a way to avoid this.
 
 ;;; Code:
 
-(require 'cl-lib)
-
 (defun acronym (phrase)
   "Convert a PHRASE into its acronym."
-  (interactive "MEnter your phrase: ")
-  (let
-      ;; Initialize with first letter of the phrase:
-      ((result (list (string-to-char phrase))))
-    ;; Create a temp buffer:
-    (with-temp-buffer
-      ;; Put the phrase into the temp buffer:
-      (insert phrase)
-      (goto-char (point-min))
-      ;; Loop until the end of buffer, or until there is no more word:
-      (while (and (< (point) (point-max))
-		  (re-search-forward "[ -]+" nil t))
-	;; Add the first (capitalized) letter in the result:
-	(setq result (cons
-		      (capitalize
-		       (string-to-char (substring phrase (1- (point)))))
-		      result))))
-    ;; Return result:
+  (let* ((current-phrase phrase)
+         (initial (string-to-char current-phrase))
+         (result (list initial)))
+    (while (string-match "[ -]+" current-phrase)
+      ;; Search for the next initial (which follows a whitespace):
+      (setq initial (1+ (string-match "[ -]+" current-phrase)))
+      ;; Add this to the list of initials:
+      (setq result (cons (capitalize (elt current-phrase initial)) result))
+      ;; Shorten the current phrase:
+      (setq current-phrase (substring current-phrase initial)))
     (apply 'string (reverse result))))
 
 (provide 'acronym)
