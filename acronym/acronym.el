136976b4464ecffdd40ba7b9f7996e88f2119641
f-santos <frederic.santos@u-bordeaux.fr>
2020-06-26 09:10:36 +0200

    Simplified acronym.el

--- acronym/acronym.el
+++ acronym/acronym.el
@@ -1,23 +1,19 @@
 ;;; acronym.el --- Acronym (exercism)
 
 ;;; Commentary:
-;;; There is a repeated call (string-match "[ -]+" current-phrase)
-;;; I've (not yet) found a way to avoid this.
 
 ;;; Code:
 
 (defun acronym (phrase)
   "Convert a PHRASE into its acronym."
-  (let* ((current-phrase phrase)
-         (initial (string-to-char current-phrase))
-         (result (list initial)))
-    (while (string-match "[ -]+" current-phrase)
-      ;; Search for the next initial (which follows a whitespace):
-      (setq initial (1+ (string-match "[ -]+" current-phrase)))
-      ;; Add this to the list of initials:
-      (setq result (cons (capitalize (elt current-phrase initial)) result))
-      ;; Shorten the current phrase:
-      (setq current-phrase (substring current-phrase initial)))
+  (let ((length-phrase (length phrase))
+        (result (list (elt phrase 0)))
+        (start-index 0))
+    (while (string-match "[ -]+" phrase start-index)
+      ;; Index where the next initial appears:
+      (setq start-index (match-end 0))
+      ;; Push this character into the list of initials:
+      (push (capitalize (elt phrase start-index)) result))
     (apply 'string (reverse result))))
 
 (provide 'acronym)
